75b1e07b18ba1e0514ef486875d391528ad7a1ca
Michael Wittig <michael.wittig@tullius-walden.com>
2013-04-30 09:14:14 +0200

    improved schemas.

--- js/render.js
+++ js/render.js
@@ -1,49 +1,19 @@
-/**
- * @param file Path to the json file (e.g. test.json or http://domain.de/test.json
- * @param cbBefore function(restdoc) executed before rendering happens (can be undefined)
- * @param contentElement element where the content is inserted (e. g. $("#test"))
- * @param cbAfter function(restdoc) executed after rendering happened (can be undefined)
- */
-function render(file, cbBefore, contentElement, cbAfter) {
-	$.ajax({
-		url : file,
-		success : function (restdoc) {
-			if (typeof cbBefore === "function") {
-				cbBefore(restdoc);
-			}
-			var output = "";
-			output += createSchemaSection(restdoc.schemas);
-			output += createHeaderSection(restdoc.headers);
-			output += createResourceSection(restdoc.resources);
-			output += createParamSection(restdoc.params);
-			contentElement.html(output);
-			if (typeof cbAfter === "function") {
-				cbAfter(restdoc);
-			}
-		},
-		error : function (e) {
-			contentElement.html("Error loading RestDoc: " + e);
-		},
-		dataType : "json"
-	});
-}
-
 function crateSchemaList(restdoc, appendElement) {
 	var output = "";
 	var i = 0;
 	for (var id in restdoc.schemas) {
 		if (restdoc.schemas.hasOwnProperty(id)) {
-			var schema = restdoc.schemas[id];
-			output += "<li><a href=\"#schema-" + i +"\">" + id + "</a></li>";
+			var s = id.split("/");
+			output += "<li><a href=\"#schema-" + i +"-anchor\">" + s[s.length-1] + "</a></li>";
 			i += 1;
 		}
 	}
-	appendElement.append(output);
+	appendElement.after(output);
 }
 
 function crateResourceList(restdoc, appendElement) {
-	var output = Mustache.render("{{#resources}}<li><a href=\"#resource-{{id}}\" data-toggle=\"collapse\" data-parent=\"#accordion-res\">{{path}}</a></li>{{/resources}}", restdoc);
-	appendElement.append(output);
+	var output = Mustache.render("{{#resources}}<li><a href=\"#resource-{{id}}-anchor\">{{path}}</a></li>{{/resources}}", restdoc);
+	appendElement.after(output);
 }
 
 function crateParamList(restdoc, appendElement) {
@@ -51,16 +21,15 @@ function crateParamList(restdoc, appendElement) {
 	var i = 0;
 	for (var id in restdoc.params) {
 		if (restdoc.params.hasOwnProperty(id)) {
-			var param = restdoc.params[id];
-			output += "<li><a href=\"#param-" + i +"\">" + id + "</a></li>";
+			output += "<li><a href=\"#param-" + i +"-anchor\">" + id + "</a></li>";
 			i += 1;
 		}
 	}
-	appendElement.append(output);
+	appendElement.after(output);
 }
 
 function createSchemaSection(schemas) {
-	var html = '<h2>Schemas</h2><div class="accordion" id="accordion-res">';
+	var html = '<h2 id="schemas-anchor">Schemas</h2><div class="accordion" id="accordion-res">';
 	var i = 0;
 	for (var id in schemas) {
 		if (schemas.hasOwnProperty(id)) {
@@ -72,29 +41,49 @@ function createSchemaSection(schemas) {
 	return html;
 }
 
+function createInlineSchema(schema) {
+	var html = "";
+	if (schema.type === "object") {
+		html += "<ul>";
+		for (var id in schema.properties) {
+			if (schema.properties.hasOwnProperty(id)) {
+				var property = schema.properties[id];
+				if (property.type === "array") {
+					html += "<li><code>" + id + "</code>: " + property.type + "[" + property.items.type + "]";
+					html += createInlineSchema(property);
+					html += "</li>";
+				} else if (property.type === "object") {
+					html += "<li><code>" + id + "</code>: " + property.type;
+					html += createInlineSchema(property);
+					html += "</li>";
+				} else {
+					html += "<li><code>" + id + "</code>: " + property.type + "</li>";
+				}
+			}
+		}
+		html += "</ul>";
+	} else if (schema.type === "array") {
+		if (schema.items.type === "array" || schema.items.type === "object") {
+			html += createInlineSchema(schema.items);
+		}
+	}
+	return html;
+}
 function createSchema(i, schemaURI, schema) {
-	var html = '<div class="accordion-group">'
-		+ '<div class="accordion-heading">'
-			+ '<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion-res" href="#schema-' + i + '">'
-			+ '<h4>' + schemaURI + ' <i class="icon-resize-vertical icon-4x"></i></h4>'
-			+ '</a>'
-		+ '</div>';
+	var html = '<div class="accordion-group" id="schema-' + i + '-anchor">' +
+		'<div class="accordion-heading">' +
+			'<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion-res" href="#schema-' + i + '">' +
+			'<h4>' + schemaURI + ' <i class="icon-resize-vertical icon-4x"></i></h4>' +
+			'</a>' +
+		'</div>';
 
 	html += '<div id="schema-' + i + '" class="accordion-body collapse"><div class="accordion-inner">';
 	if (schema.type === "url") {
 		html += '<h5>URL</h5>';
 		html += '<p>' + schema.url + '</p>';
 	} else if (schema.type === "inline") {
-		html += '<h5>Type</h5>';
-		html += '<p>' + schema.schema.type + '</p>';
-		html += '<h5>Properties</h5><ul>';
-		for (var id in schema.schema.properties) {
-			if (schema.schema.properties.hasOwnProperty(id)) {
-				var property = schema.schema.properties[id];
-				html += "<li><code>" + id + "</code>: " + property.type + "</li>"
-			}
-		}
-		html += "</ul>";
+		html += '<h5>Schema</h5>';
+		html += createInlineSchema(schema.schema);
 	} else {
 		html += "<p>Unsupported type " + schema.type + "</p>";
 	}
@@ -104,10 +93,10 @@ function createSchema(i, schemaURI, schema) {
 
 function createHeaderSection(headers) {
 	var html = '<div class="restdoc-headers" id="headers">';
-	html += '<h2>Global headers</h2>';
-	html += '<a name="header-request"></a>';
+	html += '<h2 id="headers-anchor">Global headers</h2>';
+	html += '<a name="headers-request-anchor"></a>';
 	html += createHeaderList('h3', 'Request', headers.request);
-	html += '<a name="header-response"></a>';
+	html += '<a name="headers-response-anchor"></a>';
 	html += createHeaderList('h3', 'Response', headers.response);	
 	html += '</div>';
 	return html;
@@ -116,20 +105,23 @@ function createHeaderSection(headers) {
 function createHeaderList(st, title, headers) {
 	var html = '<'+st+'>' + title + '</'+st+'>';
 	html += '<ul>';
+	var req;
 	for (var prop in headers) {
-		if (headers[prop].required && headers[prop].required === true) {
-			var req = '';
-		} else {
-			var req = ' <small><em>(optional)</em></small>';
+		if (headers.hasOwnProperty(prop)) {
+			if (headers[prop].required && headers[prop].required === true) {
+				req = '';
+			} else {
+				req = ' <small><em>(optional)</em></small>';
+			}
+			html += listItem(prop, headers[prop].description + req);
 		}
-		html += listItem(prop, headers[prop].description + req);	
 	}
 	html += '</ul>';
 	return html;
 }
 
 function createResourceSection(data) {
-	var html = '<h2>Resources</h2><div class="accordion" id="accordion-res">';
+	var html = '<h2 id="resources-anchor">Resources</h2><div class="accordion" id="accordion-res">';
 	
 	for (var i = 0; i < data.length; i++) {
 		html += createResource(data[i]);
@@ -141,13 +133,13 @@ function createResourceSection(data) {
 function createResource(res) {
 	var desc = res.description ? res.description : 'No description';
 	
-	var html = '<div class="accordion-group">';
+	var html = '<div class="accordion-group" id="resource-' + res.id + '-anchor">';
 	html += '<div class="accordion-heading">';
 	html += '<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion-res" href="#resource-' + res.id + '">';
 	html += '<h4>' + res.path + ': ' + desc + ' <i class="icon-resize-vertical icon-4x"></i></h4></a></div>';
 	
 	html += '<div id="resource-' + res.id + '" class="accordion-body collapse"><div class="accordion-inner">';
-	
+
 	html += '<h5>Identifier</h5>';
 	html += '<p>' + res.id + '</p>';
 	
@@ -156,10 +148,12 @@ function createResource(res) {
 	
 	html += '<h5>Methods</h5><div class="accordion" id="accordion-'+res.id+'-method">';
 	for (var prop in res.methods) {
-		html += createResourceMethodSection(res.id, prop, res.methods[prop]);
+		if (res.methods.hasOwnProperty(prop)) {
+			html += createResourceMethodSection(res.id, prop, res.methods[prop]);
+		}
 	}
 	html += '</div>'; // Method accordion
-	
+
 	html += '</div></div></div>';
 	return html;
 }
@@ -168,15 +162,17 @@ function createResourceParamSection(params) {
 	var html = '<div class="restdoc-params well well-small">';
 	html += '<ul>';
 	for (var prop in params) {
-		var req = '';
-		if (params[prop].validations) {
-			for (var i = 0; i < params[prop].validations.length; i++) {
-				if (params[prop].validations[i].type === 'match') {
-					req = ' (<code>' + params[prop].validations[i].pattern + '</code>)';		
+		if (params.hasOwnProperty(prop)) {
+			var req = '';
+			if (params[prop].validations) {
+				for (var i = 0; i < params[prop].validations.length; i++) {
+					if (params[prop].validations[i].type === 'match') {
+						req = ' (<code>' + params[prop].validations[i].pattern + '</code>)';
+					}
 				}
 			}
+			html += listItem(prop, params[prop].description + req);
 		}
-		html += listItem(prop, params[prop].description + req);
 	}
 	html += '</ul></div>';
 	return html;
@@ -223,26 +219,30 @@ function createAccept(title, accepts) {
 
 function createStatusCodes(codes) {
 	var html = '<h6>Return codes</h6><ul>';
-	for (var code in codes) { 
-		html += listItem(code, codes[code]);
+	for (var code in codes) {
+		if (codes.hasOwnProperty(code)) {
+			html += listItem(code, codes[code]);
+		}
 	}
 	html += '</ul>';
 	return html;
 }
 
 function createParamSection(params) {
-	var html = '<h2>Params</h2>';
+	var html = '<h2 id="params-anchor">Params</h2>';
 	html += '<ul>';
 	for (var prop in params) {
-		var req = '';
-		if (params[prop].validations) {
-			for (var i = 0; i < params[prop].validations.length; i++) {
-				if (params[prop].validations[i].type === 'match') {
-					req = ' (<code>' + params[prop].validations[i].pattern + '</code>)';
+		if (params.hasOwnProperty(prop)) {
+			var req = '';
+			if (params[prop].validations) {
+				for (var i = 0; i < params[prop].validations.length; i++) {
+					if (params[prop].validations[i].type === 'match') {
+						req = ' (<code>' + params[prop].validations[i].pattern + '</code>)';
+					}
 				}
 			}
+			html += listItem(prop, params[prop].description + req);
 		}
-		html += listItem(prop, params[prop].description + req);
 	}
 	html += '</ul>';
 	return html;
@@ -255,4 +255,34 @@ function listItem(title, text) {
 	}
 	html += '</li>';
 	return html;
+}
+
+/**
+ * @param file Path to the json file (e.g. test.json or http://domain.de/test.json
+ * @param cbBefore function(restdoc) executed before rendering happens (can be undefined)
+ * @param contentElement element where the content is inserted (e. g. $("#test"))
+ * @param cbAfter function(restdoc) executed after rendering happened (can be undefined)
+ */
+function render(file, cbBefore, contentElement, cbAfter) {
+	$.ajax({
+		"url": file,
+		"success": function (restdoc) {
+			if (typeof cbBefore === "function") {
+				cbBefore(restdoc);
+			}
+			var output = "";
+			output += createSchemaSection(restdoc.schemas);
+			output += createHeaderSection(restdoc.headers);
+			output += createResourceSection(restdoc.resources);
+			output += createParamSection(restdoc.params);
+			contentElement.html(output);
+			if (typeof cbAfter === "function") {
+				cbAfter(restdoc);
+			}
+		},
+		"error": function (e) {
+			contentElement.html("Error loading RestDoc: " + e);
+		},
+		"dataType": "json"
+	});
 }
