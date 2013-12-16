0b0b0a2a487669debf6d1d7fdda8afc3f0a0c9f6
Oliver Kopp <kopp.dev@gmail.com>
2013-12-16 18:06:12 +0100

    Fixes typos

--- js/restdoc-render.js
+++ js/restdoc-render.js
@@ -14,13 +14,13 @@
    limitations under the License.
 */
 
-// AMD and non-AMD compatiblitiy insired by http://tkareine.org/blog/2012/08/11/why-javascript-needs-module-definitions/ and https://github.com/blueimp/jQuery-File-Upload/blob/9.5.0/js/jquery.fileupload.js
+// AMD and non-AMD compatibility inspired by http://tkareine.org/blog/2012/08/11/why-javascript-needs-module-definitions/ and https://github.com/blueimp/jQuery-File-Upload/blob/9.5.0/js/jquery.fileupload.js
 (function(factory) {
   if (typeof define === 'function' && define.amd) {
     // Register as named AMD module. Anonymous registering causes "Mismatched anonymous define() module" in requirejs 2.9.1 when script is loaded explicitly and then loaded with requirejs
     define("restdoc-render", ['jquery'], factory);
   } else {
-    // traditional browser loading: build restdoc-renderer object "renderer" without dependencies and inject it into window object
+    // traditional browser loading: build object with directly referencing the global jQuery object and inject it into window object
     window.renderer = factory(window.jQuery);
   }
 }(function($) {
